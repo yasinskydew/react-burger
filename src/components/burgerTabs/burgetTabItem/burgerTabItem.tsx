d3b09b4b4f55f2fba01179998c946a984e4832d7
Vladimir Yasinsky <yasinsky2015@gmail.com>
2025-05-27 22:16:37 +0300

    fix: fix scrollbar flow

--- src/components/burgerTabs/burgetTabItem/burgerTabItem.tsx
+++ src/components/burgerTabs/burgetTabItem/burgerTabItem.tsx
@@ -10,8 +10,15 @@ interface BurgerTabItemProps {
 
 export default function BurgerTabItem({ children, onActive }: BurgerTabItemProps) {
     const activeTab = useSelector((state: ApplicationState) => state.tabs.activeTab);
+
+    const onClick = () => {
+        onActive(children.id);
+        const element = document.querySelector(`[data-tab-id="${children.id}"]`);
+        if(!element) return;
+        element.scrollIntoView({ behavior: 'smooth' });
+    }
     return (
-        <Tab active={activeTab.id === children.id} value={children.id} onClick={() => onActive(children.id)}>
+        <Tab active={activeTab.id === children.id} value={children.id} onClick={onClick}>
             {children.title}
         </Tab>
     )
