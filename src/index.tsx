751031a1c5e3202f589b0cfb28cc782159ece59b
Vladimir Yasinsky <yasinsky2015@gmail.com>
2025-07-11 16:03:47 +0300

    fix: fix routing for gh-pages

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/react-burger">
         <App />
       </BrowserRouter>
     </Provider>
